a792a8ebebc4ce14d169a4f9d4d5b362b098d12b
master <allen.abigail719@gmail.com>
2023-04-30 16:12:52 -0700

    updated some styling

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import { EmailAuthProvider, GoogleAuthProvider } from 'firebase/auth';
+import { EmailAuthProvider } from 'firebase/auth';
 import { Button, CircularProgress, Container, Dialog, Typography } from '@mui/material';
 import { useAuth } from '../firebase/auth';
 import { auth } from '../firebase/firebase';
@@ -12,47 +12,48 @@ const REDIRECT_PAGE = '/dashboard';
 
 // Configure FirebaseUI.
 const uiConfig = {
-  signInFlow: 'popup',
-  signInSuccessUrl: REDIRECT_PAGE,
-  signInOptions: [
-    EmailAuthProvider.PROVIDER_ID,
-  ],
+signInFlow: 'popup',
+signInSuccessUrl: REDIRECT_PAGE,
+signInOptions: [
+  EmailAuthProvider.PROVIDER_ID,
+],
 };
 
 export default function Home() {
-  const { authUser, isLoading } = useAuth();
-  const router = useRouter();
-  const [login, setLogin] = useState(false);
+const { authUser, isLoading } = useAuth();
+const router = useRouter();
+const [login, setLogin] = useState(false);
 
-  useEffect(() => {
-    if (!isLoading && authUser) {
-      router.push(REDIRECT_PAGE);
-    }
-  }, [authUser, isLoading])
+useEffect(() => {
+  if (!isLoading && authUser) {
+    router.push(REDIRECT_PAGE);
+  }
+}, [authUser, isLoading])
 
-  return ((isLoading || (!isLoading && !!authUser)) ? 
-    <CircularProgress color="inherit" sx={{ marginLeft: '50%', marginTop: '25%' }}/>
-    :
-    <div>
-      <Head>
-        <title>Cash Tracker</title>
-      </Head>
+return ((isLoading || (!isLoading && !!authUser)) ? 
+  <CircularProgress color="inherit" sx={{ marginLeft: '50%', marginTop: '25%' }}/>
+  :
+  <div>
+    <Head>
+    <link rel="icon" href="/favicon.ico" />
+      <title>Cash Tracker</title>
+    </Head>
 
-      <main>
-        <Container className={styles.container}>
-          <Typography variant="h1">Welcome to Cash Tracker!</Typography>
-          <Typography variant="h2">Add, view, edit, and delete expenses</Typography>
-          <div className={styles.buttons}>
-            <Button variant="contained" color="secondary"
-                    onClick={() => setLogin(true)}>
-              Login / Register
-            </Button>
-          </div>
-          <Dialog onClose={() => setLogin(false)} open={login}>
-            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth}/>
-          </Dialog>
-        </Container>
-      </main>
-    </div>
-  )
+    <main>
+      <Container className={styles.container}>
+        <Typography variant="h1">Welcome to Cash Tracker!</Typography>
+        <Typography variant="h2">Track your cash like a boss, not like a lost cause</Typography>
+        <div className={styles.buttons}>
+          <Button variant="contained" color="secondary"
+                  onClick={() => setLogin(true)}>
+            Login / Sign Up
+          </Button>
+        </div>
+        <Dialog onClose={() => setLogin(false)} open={login}>
+          <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth}/>
+        </Dialog>
+      </Container>
+    </main>
+  </div>
+)
 }
